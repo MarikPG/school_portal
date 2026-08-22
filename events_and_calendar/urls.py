e3488247c900baf792837df0fdbb297bbd3733f5
MarkPG <ytmablo@gmail.com>
2026-08-22 12:32:03 +0300

    Додано EventDetailView і template

--- events_and_calendar/urls.py
+++ events_and_calendar/urls.py
@@ -6,7 +6,7 @@ app_name = 'events_and_calendar'
 urlpatterns = [
     path('', views.EventListView.as_view(), name='event_list'),
     path('create/', views.EventListView.as_view(), name='event_create'),
-    path('<int:pk>/', views.EventListView.as_view(), name='event_detail'),
-    path('<int:pk>/update/', views.EventListView.as_view(), name='event_update'),
-    path('<int:pk>/delete/', views.EventListView.as_view(), name='event_delete'),
+    path('<int:pk>/', views.EventDetailView.as_view(), name='event_detail'),
+    path('<int:pk>/update/', views.EventListView.as_view(), name='event_update'), 
+    path('<int:pk>/delete/', views.EventListView.as_view(), name='event_delete'), 
 ]
--- events_and_calendar/views.py
+++ events_and_calendar/views.py
@@ -1,7 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Event
 
 class EventListView(ListView):
     model = Event
     template_name = 'events_and_calendar/event_list.html'
     context_object_name = 'events'
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = 'events_and_calendar/event_detail.html'
+    context_object_name = 'event'
