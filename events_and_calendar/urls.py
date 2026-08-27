71bef95a655dbabc43d31944c76af2f4ed83c2b4
MarkPG <ytmablo@gmail.com>
2026-08-27 15:50:26 +0300

    Додано EventUpdateView

--- events_and_calendar/urls.py
+++ events_and_calendar/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.EventListView.as_view(), name='event_list'),
     path('create/', views.EventCreateView.as_view(), name='event_create'),
     path('<int:pk>/', views.EventDetailView.as_view(), name='event_detail'),
-    path('<int:pk>/update/', views.EventListView.as_view(), name='event_update'), 
-    path('<int:pk>/delete/', views.EventListView.as_view(), name='event_delete'), 
+    path('<int:pk>/update/', views.EventUpdateView.as_view(), name='event_update'),
+    path('<int:pk>/delete/', views.EventListView.as_view(), name='event_delete'),  # тимчасово
 ]
--- events_and_calendar/views.py
+++ events_and_calendar/views.py
@@ -1,6 +1,6 @@
 from django import forms
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Event
 
 class EventListView(ListView):
@@ -32,3 +32,23 @@ class EventCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class EventUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Event
+    template_name = 'events_and_calendar/event_form.html'
+    fields = ['title', 'description', 'start_time', 'end_time', 'location']
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields['start_time'].widget = forms.widgets.DateTimeInput(
+            attrs={'type': 'datetime-local'}
+        )
+        form.fields['end_time'].widget = forms.widgets.DateTimeInput(
+            attrs={'type': 'datetime-local'}
+        )
+        return form
+
+    def test_func(self):
+        event = self.get_object()
+        user = self.request.user
+        return user == event.author or user.is_staff or user.is_superuser
